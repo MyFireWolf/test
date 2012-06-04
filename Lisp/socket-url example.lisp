ed7d585d2a342653832019700d5e01286b2e7f81
yexiaoting <522828368@qq.com>
2012-06-04 14:18:08 +0800

    modify

--- Lisp/socket-url example.lisp
+++ Lisp/socket-url example.lisp
@@ -1,20 +1,31 @@
-
-(setq my-socket (ccl:make-socket
-                 :address-family :internet
-                 :type :stream
-                 :connect :active                 
-                 :format :text
-                 :local-port 20000
-                 :reuse-address t))
-
-
+;;利用 socket 连接服务器   包括连接http 
+;;(setq my-socket (ccl:make-socket
+;;                 :address-family :internet
+;;                 :type :stream
+;;                 :connect :active                 
+;;                 :format :text
+;;                 :local-port 20000
+;;                 :reuse-address t))
 
 
+;;(setq a (ccl:make-socket :type :stream :connect :active :remote-host "www.qq.com" :remote-port 80))
+;;(ccl:stream-write-string a "GET / HTTP/1.1
+;;Connection:Close
+;;
+;;")
+;;(finish-output a)
+;;(ccl:stream-read-char a)
 
-(setq a (ccl:make-socket :type :stream :connect :active :remote-host "170.135.130.174" :remote-port 80))
-(ccl:stream-write-string a "GET / HTTP/1.1
-Connection:Close
+(defun httpconn (url port math)
+  (let (
+        (sock (ccl:make-socket :type :stream
+                               :connect :active
+                               :remote-host url
+                               :remote-port port))
+        )
+    (ccl:stream-write-string sock math)
+    (finish-output sock)
+    (ccl:stream-read-char sock)
 
-")
-(finish-output a)
-(ccl:stream-read-char a)
+    )  
+  )
